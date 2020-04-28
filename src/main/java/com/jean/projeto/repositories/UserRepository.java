67eb254c383c7dcde557d4db9ca9c08397ade5d0
Jean Carlos <jean.1carlos.1@gmail.com>
2020-04-28 18:26:16 -0300

    JPA repository, injeção de dependência, database seeding

---
+++ src/main/java/com/jean/projeto/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jean.projeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jean.projeto.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}

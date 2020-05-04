fad883d766b5bf1f91161743683cf029c024b9dd
Jean Carlos <jean.1carlos.1@gmail.com>
2020-05-04 19:45:20 -0300

    Associação muitos para muitos entre Product e OrderItem

--- src/main/java/com/jean/projeto/entities/Product.java
+++ src/main/java/com/jean/projeto/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -28,12 +31,16 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
+
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", 
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 		super();
 	}
@@ -90,7 +97,15 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	}
-
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for (OrderItem order : items) {
+			set.add(order.getOrder());
+		}
+		
+		return set;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;

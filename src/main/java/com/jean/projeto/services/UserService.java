ff41af77af99a7d53f70d4ec4dc7f6c45dcb00d1
Jean Carlos <jean.1carlos.1@gmail.com>
2020-05-05 16:00:13 -0300

    User insert

--- src/main/java/com/jean/projeto/services/UserService.java
+++ src/main/java/com/jean/projeto/services/UserService.java
@@ -23,4 +23,10 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
+	
+	
 }
